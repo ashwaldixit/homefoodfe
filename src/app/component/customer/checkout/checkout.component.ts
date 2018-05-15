2ab6e0214cedc810ea574d1cea384822f8ba3471
Vinay <you@example.com>
2018-05-15 21:05:12 +0530

    Checkout ui changes

--- src/app/component/customer/checkout/checkout.component.ts
+++ src/app/component/customer/checkout/checkout.component.ts
@@ -1,27 +1,36 @@
+import { CartTotal } from './../../../model/carttotal.model';
 import { Address } from './../../../model/address.model';
 import { Component, OnInit } from '@angular/core';
 import { AddressService } from '../../../services/address.service';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
     selector: 'checkout',
     templateUrl: './checkout.component.html',
     styleUrls: ['./checkout.component.css'],
-    providers : [AddressService]
+    providers: [AddressService, CartService]
 })
 
 export class CheckoutComponent implements OnInit {
 
     customerAddressList: Address[];
+    customerCart: CartTotal;
 
-    constructor(private addressService: AddressService) { }
+    constructor(private addressService: AddressService, private cartService: CartService) { }
     ngOnInit() {
-        this.getAllAdress;
-     }
+        this.getAllAdress();
+        this.getActiveProducts();
+    }
 
     getAllAdress() {
         this.addressService.getAllAddress().subscribe(res => {
             this.customerAddressList = res;
-            console.log("Address::" + JSON.stringify(res))
+        })
+    }
+
+    getActiveProducts() {
+        this.cartService.computeCart().subscribe(res => {
+            this.customerCart = res;
         })
     }
 }
